135ed190d2264e8a0ffdf1febc8029f33ae305ad
scottzyang <scottzyang@gmail.com>
2023-02-16 13:19:20 -0600

    Add styling

--- src/Display.js
+++ src/Display.js
@@ -1,15 +1,17 @@
+import './Display.css';
+
 function Display({tipPercentage, bill, people}) {
   const perPerson = ((bill * (tipPercentage / 100) + bill) / people).toFixed(2)
   return(
-    <>
+    <div>
       <h2>Inputted Values:</h2>
       <p>Total Bill: ${parseFloat(bill).toFixed(2)}</p>
       <p>Tip Percentage: {tipPercentage}%</p>
       <p>Number of People: {people}</p>
 
       <h2>Amount per Person:</h2>
-      <p>${perPerson}</p>
-    </>
+      <p className='amount'>${perPerson}</p>
+    </div>
   )
 }
 
--- src/InputForm.js
+++ src/InputForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './InputForm.css';
 import Display from './Display'
 
 function InputForm() {
@@ -6,36 +7,43 @@ function InputForm() {
   const [tipPercentage, setTipPercentage] = useState(1)
   const [people, setPeople] = useState(1)
   return(
-    <>
+    <div className="container">
       <h1>Tip Calculator</h1>
-      <form>
-        <label for="bill">Bill Amount: </label>
-        <input 
-          id='bill'
-          type="number"
-          step='.01'
-          value={bill}
-          onChange={(e) => setBill(parseFloat(e.target.value))}
-        />
-        <br/>
-        <label for="tip">Tip Percentage: </label>
-        <input 
-          id='tip'
-          type="number"
-          value={tipPercentage}
-          onChange={(e) => setTipPercentage(parseFloat(e.target.value))}
-        />
-        <br/>
-        <label for="people">Number of People: </label>
-        <input 
-          id='people'
-          type="number"
-          value={people}
-          onChange={(e) => setPeople(parseFloat(e.target.value))}
-        />
-      </form>
-      <Display tipPercentage={tipPercentage} bill={bill} people={people} />
-    </>
+      <div className="tip-amount-container">
+        <form>
+          <div>
+            <label for="bill" className="bill-label">Bill Amount: </label>
+            <input 
+              id='bill'
+              type="number"
+              step='.01'
+              value={bill}
+              onChange={(e) => setBill(parseFloat(e.target.value))}
+            />
+          </div>
+          <div>
+            <label for="tip" className="tip-label">Tip Percentage: </label>
+            <input 
+              id='tip'
+              type="number"
+              value={tipPercentage}
+              onChange={(e) => setTipPercentage(parseFloat(e.target.value))}
+            />
+          </div>
+          <div>        
+            <label for="people" className="people-label">Number of People: </label>
+              <input 
+                id='people'
+                type="number"
+                value={people}
+                onChange={(e) => setPeople(parseFloat(e.target.value))}
+              />
+          </div>
+
+        </form>
+        <Display tipPercentage={tipPercentage} bill={bill} people={people} />
+      </div>
+    </div>
   )
 }
 
